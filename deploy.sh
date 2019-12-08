870262a8653ca8ccd012d0cf364776a586d1b0ac
凝弧 <free.ye@live.cn>
2019-12-08 18:02:36 +0800

    fix: ...

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:yefz.github.io.git master
+git push -f git@github.com:yefz/yefz.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
