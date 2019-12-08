eeddb8f89f35b60a4b863138de0e2c7580230b26
凝弧 <free.ye@live.cn>
2019-12-08 21:06:18 +0800

    fix: ...

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,14 +5,6 @@ module.exports = {
   locales: { '/': { lang: 'zh-cmn-Hans' } },
   head: [
     ['link', { rel: 'icon', href: '/icons/favicon.ico' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#000000' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#000000' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
   ],
 
